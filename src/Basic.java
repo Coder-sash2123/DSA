a13967816a76e24049990d763c61d63dac26a55d
Sashwat Shukla <sashwatshukla2019@gmail.com>
2025-03-02 16:38:56 +0530

    Intitial Commit

--- src/Basic.java
+++ src/Basic.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Basic {
     public static void main(String[] args) {
-    System.out.println("Hello World");
-
+        System.out.println("Hello World");
+        System.out.println("Hello again");
     }
 }
