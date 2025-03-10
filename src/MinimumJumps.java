a74afa6fb74218f76eead0ba33cfd5aad2737862
Sashwat Ranjan Shukla <64793540+Coder-sash2123@users.noreply.github.com>
2025-03-10 23:51:54 +0530

    Create MinimumJumps.java

---
+++ src/MinimumJumps.java
@@ -0,0 +1,8 @@
+// Geek for geeks 
+// https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
+class Solution {
+    static int minJumps(int[] arr) {
+        // code here
+    }
+}
