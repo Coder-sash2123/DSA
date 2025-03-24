f043d936124c225288ebb807aa1b26f7712f1b6f
Sashwat Ranjan Shukla <64793540+Coder-sash2123@users.noreply.github.com>
2025-03-25 00:28:08 +0530

    Update recursionReverseOfArray.java

--- src/recursionReverseOfArray.java
+++ src/recursionReverseOfArray.java
@@ -2,3 +2,23 @@
 // Geeks for geeks 
 // https://www.geeksforgeeks.org/problems/reverse-an-array/0
 
+// Normal Solution 
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int temp = 0;
+        int k = arr.length-1;
+        for(int i =0; i<=k/2;i++){
+            temp = arr[k-i];
+            arr[k-i] = arr[i];
+            arr[i] = temp;
+        }
+        // for(int i= 0; i<=k; i++){
+        //     System.out.print(arr[i]+ " ");
+        // }
+    }
+}
+
+
+// Recursion Main code 
