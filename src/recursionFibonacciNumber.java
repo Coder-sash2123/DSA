f37910f2f61dc2cfa4a6221b729c72122cccb870
Sashwat Ranjan Shukla <64793540+Coder-sash2123@users.noreply.github.com>
2025-03-29 00:33:29 +0530

    Update recursionFibonacciNumber.java

--- src/recursionFibonacciNumber.java
+++ src/recursionFibonacciNumber.java
@@ -4,8 +4,7 @@
 
 class Solution {
     public int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1; 
+        if (n <= 1) return n;   // or we can write these 3 if conditipons also to reduce runtime if(n==0) return 0; if(n ==1 || n==2) return 1; else if(n<5) return n-1;  
         return fib(n-1) + fib(n-2);  
     }
 }
